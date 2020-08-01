36c2e01da4d4d791cb7aca0e46523b8f07a9c7c4
nodirshox <n.ergashev@student.inha.uz>
2020-08-01 11:43:40 +0500

    improve Reduce number to zero

--- algorithms/reduce_number_to_zero.py
+++ algorithms/reduce_number_to_zero.py
@@ -2,15 +2,12 @@ class Solution:
     def numberOfSteps (self, num: int) -> int:
         counter = 0
 
-        while num >= 0:
-            if num == 0:
-                break
-            if num > 0:
-                if num % 2 == 0:
-                    num = num / 2
-                    counter += 1
-                else:
-                    num = num - 1
-                    counter += 1
+        while num > 0:
+            if num % 2 == 0:
+                num = num / 2
+                counter += 1
+            else:
+                num = num - 1
+                counter += 1
 
         return counter
