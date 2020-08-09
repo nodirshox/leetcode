360d8ee5e5238054e655c6827eb48419fcbb7fd8
nodirshox <n.ergashev@student.inha.uz>
2020-08-09 22:53:51 +0500

    add Unique number

---
+++ algorithms/unique_number.py
@@ -0,0 +1,28 @@
+class Solution:
+    def uniqueOccurrences(self, arr: List[int]) -> bool:
+        result = []
+        selected_nums = []
+
+        i = 0
+        is_unique = True
+
+        for num in arr:
+            j = 0
+            occurence = 1
+            if num not in selected_nums:
+                selected_nums.append(num)
+
+                for numbers in arr:
+                    if i != j:
+                        if num == numbers:
+                            occurence += 1
+                    j += 1
+                print('oc',occurence)
+                if occurence in result:
+                    is_unique = False
+                    break
+                result.append(occurence)
+
+            i += 1
+            
+        return is_unique
