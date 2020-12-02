1dce8724715d1f5af9a433e73722cbbd019f4726
Nodirbek Ergashev <mr.nodirbek77@gmail.com>
2020-12-03 00:58:22 +0500

    Added solution for steps to 0 problem.

---
+++ algorithms/steps_to_zero/README.md
@@ -0,0 +1,1 @@
+[Problem](https://leetcode.com/problems/number-of-steps-to-reduce-a-number-to-zero/)
--- algorithms/steps_to_zero/solution.py
+++ algorithms/steps_to_zero/solution.py
@@ -1,13 +1,12 @@
 class Solution:
     def numberOfSteps (self, num: int) -> int:
-        counter = 0
-
+        steps = 0
         while num > 0:
             if num % 2 == 0:
                 num = num / 2
-                counter += 1
             else:
-                num = num - 1
-                counter += 1
-
-        return counter
+                num -= 1
+            
+            steps += 1
+        
+        return steps
